044accea81273b7e5000ee766fb3b70e325932ed
fahmi <afahmi13@live.com>
2020-12-13 23:23:51 +0700

    feat: add third and fourth graph

--- data_prep.py
+++ data_prep.py
@@ -15,7 +15,10 @@ def get_dataframe():
     "Mortality rate, infant, male (per 1,000 live births)",
     "GDP (constant 2010 US$)",
     "Manufacturing, value added (constant LCU)",
-    "CO2 emissions (kg per 2010 US$ of GDP)"
+    "CO2 emissions (kt)",
+    r"Electricity production from oil, gas and coal sources (% of total)",
+    r"Electricity production from hydroelectric sources (% of total)",
+    r"Electricity production from renewable sources, excluding hydroelectric (% of total)"
   ]
 
   drop_first_and_last_row = [True] * df.shape[1]
@@ -35,7 +38,10 @@ def get_dataframe():
     11:"Mortality rate",
     259:"GDP",
     51:"Manufacturing",
-    433:"CO2 Emissions"
+    501:"CO2",
+    130:"Fossil",
+    345:"Renewable",
+    915:"Hydroelectric"
   }
 
   new_df = new_df.rename(columns=columns_name_map)
@@ -44,11 +50,10 @@ def get_dataframe():
 def get_figures():
 
   df = get_dataframe()
-
+  print(df.columns)
   # graph 1, line chart GPS & Manufacturing vs Year
   graph_one = []
-  desired_columns = ["GDP", "Manufacturing"]
-  y_val_df = df[desired_columns]
+  y_val_df = df[["GDP", "Manufacturing"]]
   x_val = df['year'].tolist()
 
   #plot data into the figure
@@ -74,8 +79,8 @@ def get_figures():
   layout_one = dict(
     title = 'GDP & Manufacturing Added Value over Years',
     xaxis = dict(title='Year'),
-    yaxis = dict(title='GDP'),
-    yaxis2 = dict(title='Manufacturing Added Value', overlaying='y', side='right')
+    yaxis = dict(title='GDP USD)'),
+    yaxis2 = dict(title='Manufacturing Added Value (USD)', overlaying='y', side='right')
   )
 
   # figure 2, scatterplot child mortality rate vs GDP
@@ -93,8 +98,69 @@ def get_figures():
 
   layout_two = dict(
     title='GDP vs Mortality Rate',
-    xaxis = dict(title='GDP'),
+    xaxis = dict(title='GDP (USD)'),
     yaxis = dict(title='Child Mortality Rate per 1000')
   )
 
-  return [dict(data=graph_one, layout=layout_one), dict(data=graph_two, layout=layout_two)]
+  # graph 3, line chart GDP & CO2 Emmisions vs Year
+  graph_three = []
+  y_val_df = df[["GDP", "CO2"]]
+  x_val = df['year'].tolist()
+
+  #plot data into the figure
+  graph_three.append(
+    go.Scatter(
+      x = x_val,
+      y = y_val_df["GDP"].tolist(),
+      mode = 'lines+markers',
+      name = "GDP"
+    )
+  )
+  graph_three.append(
+    go.Scatter(
+      x = x_val,
+      y = y_val_df["CO2"].tolist(),
+      mode = 'lines+markers',
+      name = "CO2",
+      yaxis = 'y2'
+    )
+  )
+
+  #figure configuration
+  layout_three = dict(
+    title = 'GDP & CO2 Emmisions over Years',
+    xaxis = dict(title='Year'),
+    yaxis = dict(title='GDP (USD)'),
+    yaxis2 = dict(title='CO2 Emmisions (kt)', overlaying='y', side='right')
+  )
+
+  # graph 4, line chart Arable land & Forest Area
+  graph_four = []
+  desired_col_name = ["Renewable", "Fossil", "Hydroelectric"]
+  y_val_df = df[desired_col_name]
+  x_val = df['year'].tolist()
+
+  #plot data into the figure
+  for col_name in desired_col_name:
+    graph_four.append(
+      go.Scatter(
+        x = x_val,
+        y = y_val_df[col_name].tolist(),
+        mode = 'lines+markers',
+        name = col_name
+      )
+    )
+
+  #figure configuration
+  layout_four = dict(
+    title = r'% of Electricity Production',
+    xaxis = dict(title='Year'),
+    yaxis = dict(title=r'% of Electricity Production'),
+  )
+
+  return [
+    dict(data=graph_one, layout=layout_one),
+    dict(data=graph_two, layout=layout_two),
+    dict(data=graph_three, layout=layout_three),
+    dict(data=graph_four, layout=layout_four)
+  ]
